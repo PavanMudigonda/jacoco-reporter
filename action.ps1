5aa32d89880bcb4a57f3e01b1f4cb3c33ddd0a80
Pavan Mudigonda <mnpawan@gmail.com>
2022-03-14 20:58:54 +0530

    fixed issue 1
    Fixed issue with fail below threshold. Refer Issue# 1
    
    fixes #1

--- action.ps1
+++ action.ps1
@@ -143,7 +143,7 @@ if ($inputs.fail_below_threshold -eq "true") {
         Write-ActionInfo "  * fail_below_threshold: true"
     }
     
-if ($coverage_value.coveragePercentage -lt $inputs.minimum_coverage -and $inputs.fail_below_threshold -eq "true") {
+if ($coverage_value -lt $inputs.minimum_coverage -and $inputs.fail_below_threshold -eq "true") {
         $script:stepShouldFail = $true
     }
 
