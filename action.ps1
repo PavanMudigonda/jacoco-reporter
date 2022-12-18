72d434a78be4b4b7fe8103485bcc7e5ba37c3dbf
Pavan Mudigonda <86745613+PavanMudigondaTR@users.noreply.github.com>
2022-12-18 14:43:56 -0500

    updated

--- action.ps1
+++ action.ps1
@@ -23,24 +23,24 @@ function splitListInput { $args[0] -split ',' | % { $_.Trim() } }
 function writeListInput { $args[0] | % { Write-ActionInfo "    - $_" } }
 
 $inputs = @{
-    coverage_report_name = Get-ActionInput coverage_report_name
+    coverage_report_name  = Get-ActionInput coverage_report_name
     coverage_report_title = Get-ActionInput coverage_report_title
     coverage_results_path = Get-ActionInput coverage_results_path -Required
-    github_token       = Get-ActionInput github_token -Required
-    skip_check_run     = Get-ActionInput skip_check_run
-    minimum_coverage   = Get-ActionInput minimum_coverage
-    fail_below_threshold = Get-ActionInput fail_below_threshold
-    publish_only_summary = Get-ActionInput publish_only_summary
+    github_token          = Get-ActionInput github_token -Required
+    skip_check_run        = Get-ActionInput skip_check_run
+    minimum_coverage      = Get-ActionInput minimum_coverage
+    fail_below_threshold  = Get-ActionInput fail_below_threshold
+    publish_only_summary  = Get-ActionInput publish_only_summary
 }
 
 $test_results_dir = Join-Path $PWD _TMP
 Write-ActionInfo "Creating test results space"
 mkdir $test_results_dir
 Write-ActionInfo $test_results_dir
-$script:coverage_report_path = Join-Path $test_results_dir coverage-results.md
+$script:coverage_report_path  = Join-Path $test_results_dir coverage-results.md
 $script:coverage_summary_path = Join-Path $test_results_dir coverage-summary.md
-$script:publish_only_summary = $inputs.publish_only_summary
-$script:skip_check_run = $inputs.skip_check_run
+$script:publish_only_summary  = $inputs.publish_only_summary
+$script:skip_check_run        = $inputs.skip_check_run
 
 # Feature 1
 function Build-CoverageReport
@@ -114,68 +114,6 @@ function Build-SummaryReport
         }
 }
 
-# Function to Publish Check Run with Neutral Status # Round 1
-function Publish-ToCheckRun {
-    param(
-        [string]$reportData,
-        [string]$reportName,
-        [string]$reportTitle
-    )
-
-    Write-ActionInfo "Publishing Report to GH Workflow"
-
-    $ghToken = $inputs.github_token
-    $ctx = Get-ActionContext
-    $repo = Get-ActionRepo
-    $repoFullName = "$($repo.Owner)/$($repo.Repo)"
-
-    Write-ActionInfo "Resolving REF"
-    $ref = $ctx.Sha
-    if ($ctx.EventName -eq 'pull_request') {
-        Write-ActionInfo "Resolving PR REF"
-        $ref = $ctx.Payload.pull_request.head.sha
-        if (-not $ref) {
-            Write-ActionInfo "Resolving PR REF as AFTER"
-            $ref = $ctx.Payload.after
-        }
-    }
-    if (-not $ref) {
-        Write-ActionError "Failed to resolve REF"
-        exit 1
-    }
-    Write-ActionInfo "Resolved REF as $ref"
-    Write-ActionInfo "Resolve Repo Full Name as $repoFullName"
-
-    Write-ActionInfo "Adding Check Run"
-    $url = "https://api.github.com/repos/$repoFullName/check-runs"
-    $hdr = @{
-        Accept = 'application/vnd.github+json'
-        Authorization = "token $ghToken"
-    }
-    $bdy = @{
-        name       = $reportName
-        head_sha   = $ref
-        status     = 'completed'
-        conclusion = 'neutral'
-        output     = @{
-            title   = "Code Coverage $script:coveragePercentageString"
-            summary = "This run completed at ``$([datetime]::Now)``"
-            text    = $ReportData
-        }
-    }
-    Write-ActionInfo "$hdr"
-    Write-ActionInfo $url
-    Write-ActionInfo "$bdy"
-
-    $response = Invoke-WebRequest -Headers $hdr $url -Method Post -Body ($bdy | ConvertTo-Json)
-    # Grab Check ID
-    # $checkId = ( ConvertFrom-Json $response.Content ).id
-    # $checkUrl = ( ConvertFrom-Json $response.Content ).url
-    # Write-ActionInfo "Check ID: $checkId"
-    # Write-ActionInfo "Check ID: $checkUrl"
-    # $script:checkUrl = $checkUrl
-    # $script:checkId = $checkId
-}
 
 function Parse-CoverageXML {
     # Parse XML
@@ -192,24 +130,24 @@ function Format-Percentage {
     # Format Percentage
     if ($script:missedLines -eq 0)
         {
-        $coveragePercentage = 100
-        $script:coveragePercentage = 100
-        Write-Output "Coverage: $coveragePercentage"
-        $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage/100)
+            $coveragePercentage = 100
+            $script:coveragePercentage = 100
+            Write-Output "Coverage: $coveragePercentage"
+            $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage/100)
         }
     elseif ($script:coveredLines -eq 0)
         {
-        $coveragePercentage = 0
-        $script:coveragePercentage = 0
-        Write-Output "Coverage: $coveragePercentage"
-        $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage)
+            $coveragePercentage = 0
+            $script:coveragePercentage = 0
+            Write-Output "Coverage: $coveragePercentage"
+            $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage)
         }
     elseif ($coveredLines -eq 0 -and $missedLines -eq 0)
         {
-        $coveragePercentage = 0
-        $script:coveragePercentage = 0
-        Write-Output "Coverage: $coveragePercentage"
-        $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage)
+            $coveragePercentage = 0
+            $script:coveragePercentage = 0
+            Write-Output "Coverage: $coveragePercentage"
+            $script:coveragePercentageString = "{0:p2}" -f ($coveragePercentage)
         }
     else
         {
@@ -230,7 +168,6 @@ function Set-Output {
     Set-ActionVariable -Name covered_lines -Value ($script:coveredLines)
     Set-ActionVariable -Name missed_lines -Value ($script:missedLines)
     Set-ActionVariable -Name total_lines -Value ($coveredLines+$missedLines)
-
     Set-ActionOutput -Name coveragePercentageString -Value ($script:coveragePercentageString)
     Set-ActionOutput -Name coveragePercentage -Value ($script:coveragePercentage)
     Set-ActionOutput -Name coverage_percentage -Value ($script:coveragePercentage)
@@ -338,6 +275,69 @@ function Enforce-QualityGate {
 # }
 
 # Publishing Report to GH Workflow
+# Function to Publish Check Run with Neutral Status # Round 1
+function Publish-ToCheckRun {
+    param(
+        [string]$reportData,
+        [string]$reportName,
+        [string]$reportTitle
+    )
+
+    Write-ActionInfo "Publishing Report to GH Workflow"
+
+    $ghToken = $inputs.github_token
+    $ctx = Get-ActionContext
+    $repo = Get-ActionRepo
+    $repoFullName = "$($repo.Owner)/$($repo.Repo)"
+
+    Write-ActionInfo "Resolving REF"
+    $ref = $ctx.Sha
+    if ($ctx.EventName -eq 'pull_request') {
+        Write-ActionInfo "Resolving PR REF"
+        $ref = $ctx.Payload.pull_request.head.sha
+        if (-not $ref) {
+            Write-ActionInfo "Resolving PR REF as AFTER"
+            $ref = $ctx.Payload.after
+        }
+    }
+    if (-not $ref) {
+        Write-ActionError "Failed to resolve REF"
+        exit 1
+    }
+    Write-ActionInfo "Resolved REF as $ref"
+    Write-ActionInfo "Resolve Repo Full Name as $repoFullName"
+
+    Write-ActionInfo "Adding Check Run"
+    $url = "https://api.github.com/repos/$repoFullName/check-runs"
+    $hdr = @{
+        Accept = 'application/vnd.github+json'
+        Authorization = "token $ghToken"
+    }
+    $bdy = @{
+        name       = $reportName
+        head_sha   = $ref
+        status     = 'completed'
+        conclusion = $script:outcome
+        output     = @{
+            title   = "Code Coverage $script:coveragePercentageString"
+            summary = "This run completed at ``$([datetime]::Now)``"
+            text    = $ReportData
+        }
+    }
+    Write-ActionInfo "$hdr"
+    Write-ActionInfo $url
+    Write-ActionInfo "$bdy"
+
+    Invoke-WebRequest -Headers $hdr $url -Method Post -Body ($bdy | ConvertTo-Json)
+    # Grab Check ID
+    # $checkId = ( ConvertFrom-Json $response.Content ).id
+    # $checkUrl = ( ConvertFrom-Json $response.Content ).url
+    # Write-ActionInfo "Check ID: $checkId"
+    # Write-ActionInfo "Check ID: $checkUrl"
+    # $script:checkUrl = $checkUrl
+    # $script:checkId = $checkId
+}
+
 
 Write-ActionInfo "Publishing Report to GH Workflow"
 $coverage_results_path = $inputs.coverage_results_path
@@ -377,7 +377,7 @@ elseif ($inputs.skip_check_run -ne $true -and $inputs.publish_only_summary -ne $
 
         Publish-ToCheckRun -ReportData $coverageSummaryData -ReportName "Code Coverage: $script:coveragePercentageString" -ReportTitle $script:coverage_report_title
 
-#         Update-PRCheck -ReportData $script:coverageSummaryData -ReportName $coverage_report_name -ReportTitle $script:messageToDisplay
+#       Update-PRCheck -ReportData $script:coverageSummaryData -ReportName $coverage_report_name -ReportTitle $script:messageToDisplay
 
         Enforce-QualityGate
         # Set-ActionOutput -Name coverageSummary -Value $script:coverageSummaryData
@@ -396,7 +396,7 @@ elseif ($inputs.skip_check_run -eq $true -and $inputs.publish_only_summary -eq $
 
         Set-Output
         
-        $coverageSummary = [System.IO.File]::ReadAllText($script:coverage_summary_path)
+        # $coverageSummary = [System.IO.File]::ReadAllText($script:coverage_summary_path)
 
         Enforce-QualityGate
         # Set-ActionOutput -Name coverageSummary -Value $script:coverageSummary
@@ -412,7 +412,7 @@ else {
         
         Set-Output
         
-        $coverageSummary = [System.IO.File]::ReadAllText($script:coverage_summary_path)
+        # $coverageSummary = [System.IO.File]::ReadAllText($script:coverage_summary_path)
         
         Enforce-QualityGate
 
